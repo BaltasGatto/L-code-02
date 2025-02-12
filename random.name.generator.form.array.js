eb874fc22484e5817227213daeca5844ff3037fc
ProCare <59872662+BaltasGatto@users.noreply.github.com>
2025-02-13 00:29:07 +0200

    Add files via upload

--- random.name.generator.form.array.js
+++ random.name.generator.form.array.js
@@ -8,6 +8,14 @@ console.log(paying);
 
 const billCalculator = function () {
   const payingForTheLunch = Math.floor(Math.random() * wilPay.length);
-  return alert(wilPay[payingForTheLunch]);
+  return console.log(wilPay[payingForTheLunch]);
 };
 billCalculator();
+
+function whosPaying(names) {
+  const numerOfPeople = names.length;
+  const randomPersonPosition = Math.floor(Math.random() * numerOfPeople);
+  const randomPerson = names[randomPersonPosition];
+
+  return randomPerson + `is goging to buy lunch!`;
+}
